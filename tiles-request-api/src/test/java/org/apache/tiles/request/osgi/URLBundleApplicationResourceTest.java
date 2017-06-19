586d164e131a032a997ed2a0becd6ac9cdcb9b74
Michael Semb Wever <mck@apache.org>
2017-06-19 23:52:37 +0000

    TREQ-21: Convert IOException to FileNotFoundException on local protocols - Separate between local and remote URL protocols, check on construction time whether the URL points to a local resource - Always throw a FileNotFoundException when the URL points to a local resource and url.openConnection throws an IOException - Added system property 'tiles.remoteProtocols' which can be used to specify additional remote protocols - Added OSGi tests including PAX-EXAM dependencies
    references:
     - pull request: https://github.com/apache/tiles-request/pull/2
     - ticket: https://issues.apache.org/jira/browse/TREQ-21
    
    Contributed by Roland Hauser ( https://github.com/SourcePond )
    
    This closes #2
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tiles/request/branches/TREQ_1_0_X@1799289 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tiles-request-api/src/test/java/org/apache/tiles/request/osgi/URLBundleApplicationResourceTest.java
@@ -0,0 +1,71 @@
+/*
+ * $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.tiles.request.osgi;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.tiles.request.locale.URLApplicationResource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+
+import static org.junit.Assert.fail;
+import static org.ops4j.pax.exam.CoreOptions.bootDelegationPackage;
+import static org.ops4j.pax.exam.CoreOptions.frameworkProperty;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+
+
+/**
+ * Tests OSGi behaviour of URLApplicationResource.
+ *
+ * @version $Rev$ $Date$
+ */
+@RunWith(PaxExam.class)
+public class URLBundleApplicationResourceTest {
+
+    @Configuration
+    public Option[] configuration() throws IOException {
+        return new Option[]{
+                junitBundles(),
+                frameworkProperty("org.osgi.framework.bundle.parent").value("app"),
+                bootDelegationPackage("org.apache.tiles.request.locale"),
+        };
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void testGetLastModified() throws Exception {
+        URL url = new URL(getClass().getResource("/org/apache/tiles/request/test/locale/resource.txt"), "notExisting");
+        URLApplicationResource resource = new URLApplicationResource("/my test/path_fr.html", url);
+        resource.getLastModified();
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void testGetInputStream() throws IOException {
+        URL url = new URL(getClass().getResource("/org/apache/tiles/request/test/locale/resource.txt"), "notExisting");
+        URLApplicationResource resource = new URLApplicationResource("org/apache/tiles/request/test/locale/resource.txt", url);
+        resource.getInputStream();
+    }
+}
