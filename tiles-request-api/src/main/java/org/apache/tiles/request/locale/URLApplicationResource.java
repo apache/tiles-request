d8a57c3929630264b3e45958e2deaa80908b29f9
Michael Semb Wever <mck@apache.org>
2017-06-20 00:03:43 +0000

    `svn merge -c1799289 ^/tiles/request/branches/TREQ_1_0_X`
    TREQ-21: Convert IOException to FileNotFoundException on local protocols
    - Separate between local and remote URL protocols, check on construction time whether the URL points to a local resource
    - Always throw a FileNotFoundException when the URL points to a local resource and url.openConnection throws an IOException
    - Added system property 'tiles.remoteProtocols' which can be used to specify additional remote protocols
    - Added OSGi tests including PAX-EXAM dependencies
    
    references:
     - pull request: https://github.com/apache/tiles-request/pull/2
     - ticket: https://issues.apache.org/jira/browse/TREQ-21
    
    Contributed by Roland Hauser ( https://github.com/SourcePond )
    
    This closes #2
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tiles/request/trunk@1799291 13f79535-47bb-0310-9956-ffa450edef68

--- tiles-request-api/src/main/java/org/apache/tiles/request/locale/URLApplicationResource.java
+++ tiles-request-api/src/main/java/org/apache/tiles/request/locale/URLApplicationResource.java
@@ -23,6 +23,7 @@ package org.apache.tiles.request.locale;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.JarURLConnection;
@@ -30,10 +31,15 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static java.lang.System.getProperty;
+import static java.util.Collections.unmodifiableSet;
 
 /**
  * A {@link PostfixedApplicationResource} that can be accessed through a URL.
@@ -42,12 +48,73 @@ import org.slf4j.LoggerFactory;
  */
 
 public class URLApplicationResource extends PostfixedApplicationResource {
+    /**
+     * System parameter to specify additional remote protocols. If a url has a remote protocol, then any
+     * {@link IOException} will be thrown directly. If a url has a local protocol, then any {@link IOException}
+     * will be caught and transformed into a {@link FileNotFoundException}.
+     */
+    static final String REMOTE_PROTOCOLS_PROPERTY = "tiles.remoteProtocols";
     private static final Logger LOG = LoggerFactory.getLogger(URLApplicationResource.class);
+    private static final Set<String> REMOTE_PROTOCOLS;
+
+    static {
+        REMOTE_PROTOCOLS = initRemoteProtocols();
+    }
+
+    /**
+     * Creates an unmodifiable set of <em>remote</em> protocols which are used in {@link URL} objects, see {@link URL#getProtocol()}.
+     * A url with a remote protocol establishes a network connection when its {@link URL#openConnection()} is being called.
+     * The set will always contain the built-in remote protocols below:
+     * <ul>
+     *  <li><a href="http://hg.openjdk.java.net/jdk8/jdk8/jdk/file/687fd7c7986d/src/share/classes/sun/net/www/protocol/ftp">ftp</a></li>
+     *  <li><a href="http://hg.openjdk.java.net/jdk8/jdk8/jdk/file/687fd7c7986d/src/share/classes/sun/net/www/protocol/http">http</a></li>
+     *  <li><a href="http://hg.openjdk.java.net/jdk8/jdk8/jdk/file/687fd7c7986d/src/share/classes/sun/net/www/protocol/https">https</a></li>
+     *  <li><a href="http://hg.openjdk.java.net/jdk8/jdk8/jdk/file/687fd7c7986d/src/share/classes/sun/net/www/protocol/mailto">mailto</a></li>
+     *  <li><a href="http://hg.openjdk.java.net/jdk8/jdk8/jdk/file/687fd7c7986d/src/share/classes/sun/net/www/protocol/netdoc">netdoc</a></li>
+     * </ul>
+     * It's possible, that your environment provides additional remote protocols because of following reasons:
+     * <ul>
+     *     <li>your application server adds more remote protocols, see its documentation for further details.</li>
+     *     <li>your application supplies custom remote protocols trough its own {@link java.net.URLStreamHandlerFactory}
+     *     (see following excellent <a href="https://stackoverflow.com/questions/26363573/registering-and-using-a-custom-java-net-url-protocol">explanation</a>
+     *     for getting an idea how to do this)</li>
+     * </ul>
+     * If you need to use such extra remote protocols in Tiles, you may enhance the set via system property {@code tiles.remoteProtocols}. Suppose
+     * you need to add your custom remote protocols "foo" and "bar". To do so, add following parameter to the command line (use ";" as separator):
+     * <pre>
+     *     -Dtiles.remoteProtocols=foo;bar
+     * </pre>
+     * The resulting set will then contain the built-in protocols plus "foo" and "bar".
+     *
+     * @return Unmodifiable set of remote protocols, never {@code null}
+     */
+    static Set<String> initRemoteProtocols() {
+        Set<String> remoteProtocols = new HashSet<String>();
+        remoteProtocols.add("ftp");
+        remoteProtocols.add("http");
+        remoteProtocols.add("https");
+        remoteProtocols.add("mailto");
+        remoteProtocols.add("netdoc");
+
+        String protocolsProp = getProperty(REMOTE_PROTOCOLS_PROPERTY);
+        if (protocolsProp != null) {
+            for (String protocol : protocolsProp.split(";")) {
+                remoteProtocols.add(protocol.trim());
+            }
+        }
+        return unmodifiableSet(remoteProtocols);
+    }
+
+    private static boolean isLocal(URL url) {
+        return !REMOTE_PROTOCOLS.contains(url.getProtocol());
+    }
 
     /** the URL where the contents can be found. */
-    private URL url;
+    private final URL url;
     /** if the URL matches a file, this is the file. */
     private File file;
+    /** if the URL points to a local resource */
+    private final boolean local;
 
     /**
      * Creates a URLApplicationResource for the specified path that can be accessed through the specified URL.
@@ -61,6 +128,7 @@ public class URLApplicationResource extends PostfixedApplicationResource {
         if ("file".equals(url.getProtocol())) {
             file = getFile(url);
         }
+        local = isLocal(url);
     }
 
     /**
@@ -76,24 +144,41 @@ public class URLApplicationResource extends PostfixedApplicationResource {
         if ("file".equals(url.getProtocol())) {
             file = getFile(url);
         }
+        local = isLocal(url);
+    }
+
+    private URLConnection openConnection() throws IOException {
+        try {
+            return url.openConnection();
+        } catch (IOException e) {
+            // If the url points to a local resource but it cannot be
+            // opened, then the resource actually does not exist. In this
+            // case throw a FileNotFoundException
+            if (local) {
+                FileNotFoundException fne = new FileNotFoundException(url.toString());
+                fne.initCause(e);
+                throw fne;
+            }
+            throw e;
+        }
     }
 
     private static File getFile(URL url) {
-    	try {
-			return new File(new URI(url.toExternalForm()).getSchemeSpecificPart());
-		} catch (URISyntaxException e) {
-			LOG.debug("Cannot translate URL to file name, expect a performance impact", e);
-			return null;
-		}
+        try {
+            return new File(new URI(url.toExternalForm()).getSchemeSpecificPart());
+        } catch (URISyntaxException e) {
+            LOG.debug("Cannot translate URL to file name, expect a performance impact", e);
+            return null;
+        }
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public InputStream getInputStream() throws IOException {
         if (file != null) {
             return new FileInputStream(file);
         } else {
-            return url.openConnection().getInputStream();
+            return openConnection().getInputStream();
         }
     }
 
@@ -103,7 +188,7 @@ public class URLApplicationResource extends PostfixedApplicationResource {
         if (file != null) {
             return file.lastModified();
         } else {
-            URLConnection connection = url.openConnection();
+            URLConnection connection = openConnection();
             if (connection instanceof JarURLConnection) {
                 return ((JarURLConnection) connection).getJarEntry().getTime();
             } else {
